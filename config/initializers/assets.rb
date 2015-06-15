4942c88a14283c7eeb6fc27daf8874f776ff7d3e
Navpreet Singh <navpreet.singh.k@gmail.com>
2015-06-15 11:22:04 +0530

    modified assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap-sass-official", "assets", "fonts")
+
+Rails.application.config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
